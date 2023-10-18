e4ab0d373551ecb728752ed44f15235e94c51f51
Gullit Moran <gullitmoran@hotmail.com>
2023-10-18 17:26:47 -0500

    conectardo con backen

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSignInService {
   static GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -11,12 +12,21 @@ class GoogleSignInService {
     try {
       final GoogleSignInAccount? account = await _googleSignIn.signIn();
       final googleKey = await account!.authentication;
-      print(account);
-      print('============ ID TOKEN ===============');
-      print(googleKey.idToken);
+      //print(account);
+      //print('============ ID TOKEN ===============');
+      //print(googleKey.idToken);
 
       //TODO: Llamar un servicio REST a nuestro backend
-      // Con el ID Token
+      final signInWithGoogleEndpoint = Uri(
+          scheme: 'https',
+          host: 'auth-backen-idtoken-google.onrender.com',
+          path: '/google');
+
+      final session = await http
+          .post(signInWithGoogleEndpoint, body: {'token': googleKey.idToken});
+      print('===== BACKEND =====');
+      print(session.body);
+
       return account;
 
       // TODO: idToken
